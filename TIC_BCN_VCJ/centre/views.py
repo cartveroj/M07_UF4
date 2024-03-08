65a8ab0ec77bbe074af7d4814a86166256d35618
veronica <cartagenaveronica.19@gmail.com>
2024-03-08 16:30:16 +0100

    statics

--- TIC_BCN_VCJ/centre/views.py
+++ TIC_BCN_VCJ/centre/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
-
+from django.template import Context , loader
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    profesor = {"name":"Roger", "surname":"Sobrino"}
+    # template = loader.get_template('index.html')
+    # dades = template.render({'nombre':profesor["name"]})
+    return render(request,'index.html',{'nombre':profesor["name"],'surname':profesor["surname"]})
