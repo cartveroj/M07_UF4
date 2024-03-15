ad28e585421fab4be532d72b88ecc2e9030a8893
veronica <cartagenaveronica.19@gmail.com>
2024-03-15 16:16:42 +0100

    creacion de model

--- TIC_BCN_VCJ/centre/models.py
+++ TIC_BCN_VCJ/centre/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 100)
+    first_last_name = models.CharField(max_length=50)
+    second_last_name = models.CharField(max_length=50)
+    rol = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    course = models.CharField(max_length=50)
+    age = models.CharField(max_length=10)
+    modules = models.CharField(max_length=30)
